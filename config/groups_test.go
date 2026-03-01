ad02833ef6e9298ccde0c4f40a077549d37989fe
Veera Adithya Dittakavi <veera.adithya.dittakavi@oracle.com>
2026-02-28 20:43:38 -0600

    migrate source and dependencies to v2
    (cherry picked from commit c40a4185096c04a3a0a07546827683b11dcd23db)

--- config/groups_test.go
+++ config/groups_test.go
@@ -19,7 +19,7 @@ package config
 import (
 	"testing"
 
-	"github.com/crossplane/upjet/pkg/config"
+	"github.com/crossplane/upjet/v2/pkg/config"
 )
 
 func assertValidGroupAndKind(t *testing.T, resourceName, group, kind string) {
